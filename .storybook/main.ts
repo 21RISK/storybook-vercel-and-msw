cc4c666e6dc0f03b64fcf452490de2a751d67e66
Alex Bjorlig <alexbjorlig@icloud.com>
2024-03-04 16:03:58 +0100

    added viteFinal step

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -13,6 +13,14 @@ const config: StorybookConfig = {
 	},
 	docs: {
 		autodocs: 'tag'
-	}
+	},
+	viteFinal: async (config) => {
+        // Based on https://github.com/storybookjs/storybook/issues/25399#issuecomment-1905569873
+        if (!config.build) {
+            config.build = {};
+        }
+        config.build.target = ['es2022', 'edge89', 'firefox89', 'chrome89', 'safari15'];
+        return config;
+    },
 };
 export default config;
