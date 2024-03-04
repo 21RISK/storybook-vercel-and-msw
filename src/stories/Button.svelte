25ffab0a090fb3d7b7fd4ef643c21c486d3efe16
Alex Bjorlig <alexbjorlig@icloud.com>
2024-03-04 15:26:29 +0100

    initial commit

---
+++ src/stories/Button.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+  import './button.css';
+
+  /**
+   * Is this the principal call to action on the page?
+   */
+  export let primary = false;
+
+  /**
+   * What background color to use
+   */
+  export let backgroundColor: string | undefined = undefined;
+  /**
+   * How large should the button be?
+   */
+  export let size: 'small' | 'medium' | 'large' = 'medium';
+  /**
+   * Button contents
+   */
+  export let label: string = '';
+
+  $: mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
+
+  $: style = backgroundColor ? `background-color: ${backgroundColor}` : '';
+
+  async function handleClick() {
+    const response = await fetch('/some-route');
+    
+    const asText = await response.text();
+    text = asText;
+  }
+
+  let text = 'click to try';
+</script>
+
+<button
+  type="button"
+  class={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
+  {style}
+  on:click={handleClick}
+>
+  {text}
+</button>
